ba31d527f761bdbdfc8d169e04d3fdaf34ec39dc
justinrampengan <105626099+OrangJenius@users.noreply.github.com>
2023-08-15 12:39:58 +0700

    updateicon
    masih error pas diawal ga sampe sedetik terus jadi

--- final_project_semoga/lib/screens/pengiriman.dart
+++ final_project_semoga/lib/screens/pengiriman.dart
@@ -35,6 +35,12 @@ class _PengirimanState extends State<Pengiriman> {
 
   DateTime? now;
   String? formattedDate;
+
+// Create custom marker icons
+  BitmapDescriptor? customMarkerIcon;
+  BitmapDescriptor? sourceMarkerIcon;
+  BitmapDescriptor? destinationMarkerIcon;
+
   late SharedPreferences prefs;
 
   Future<void> initializeSharedPreferences() async {
@@ -90,6 +96,34 @@ class _PengirimanState extends State<Pengiriman> {
       print('[BackgroundFetch] configure success: $status');
     }).catchError((e) {
       print('[BackgroundFetch] configure FAILURE: $e');
+    });
+
+    // Load marker assets
+    BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: 1),
+      'assets/currentLocation.png', // Replace with your asset path
+    ).then((descriptor) {
+      setState(() {
+        customMarkerIcon = descriptor;
+      });
+    });
+
+    BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: 1),
+      'assets/sourceLocation.png', // Replace with your asset path
+    ).then((descriptor) {
+      setState(() {
+        sourceMarkerIcon = descriptor;
+      });
+    });
+
+    BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: 1),
+      'assets/destinationLocation.png', // Replace with your asset path
+    ).then((descriptor) {
+      setState(() {
+        destinationMarkerIcon = descriptor;
+      });
     });
   }
 
@@ -311,16 +345,19 @@ class _PengirimanState extends State<Pengiriman> {
                           _currentLocation!.latitude!,
                           _currentLocation!.longitude!,
                         ),
+                        icon: customMarkerIcon!,
                         infoWindow: const InfoWindow(title: 'Current Location'),
                       ),
                       Marker(
                         markerId: const MarkerId('source'),
                         position: _srcLoc,
+                        icon: sourceMarkerIcon!,
                         infoWindow: const InfoWindow(title: 'Source Location'),
                       ),
                       Marker(
                         markerId: const MarkerId('destination'),
                         position: _destLoc,
+                        icon: destinationMarkerIcon!,
                         infoWindow:
                             const InfoWindow(title: 'Destination Location'),
                       ),
